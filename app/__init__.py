f0666d0a590196bab88ad1e99276445b916bef5d
Ekene <ekeneazubuko2935@gmail.com>
2025-06-23 15:43:30 -0700

    new change

--- app/__init__.py
+++ app/__init__.py
@@ -41,9 +41,48 @@ HOBBIES = [
     }
 ]
 
+EXPERIENCES = [
+    {
+        'title': 'Student',
+        'company': 'Minerva University',
+        'date': '2023 - Present',
+        'image': './static/img/mu-image.png',
+        'alt': 'Minerva University',
+        'description': [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
+        ],
+        'reverse': False
+    },
+    {
+        'title': 'Radio Frequency Test Engineer',
+        'company': 'Underwriters Laboratories',
+        'date': 'April 2024 - August 2024',
+        'image': './static/img/ul-logo.png',
+        'alt': 'Underwriters Laboratories',
+        'description': [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
+        ],
+        'reverse': True
+    },
+    {
+        'title': 'Software Engineering Intern',
+        'company': 'UNIPORT',
+        'date': 'January 2025 - April 2025',
+        'image': './static/img/uniport-logo.png',
+        'alt': 'UNIPORT',
+        'description': [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
+        ],
+        'reverse': False
+    }
+]
+
 load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), hobbies=HOBBIES)
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), hobbies=HOBBIES, experiences=EXPERIENCES)
