7c8e4b481938b1b6e567496d53996702e7367449
Ekene <ekeneazubuko2935@gmail.com>
2025-06-29 08:23:31 -0700

    made nvabar functional

--- app/__init__.py
+++ app/__init__.py
@@ -81,7 +81,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), hobbies=HOBBIES, experiences=EXPERIENCES)
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
 @app.route('/about')
 def about():
