21a0de94ef3f65f564bb6d439d5056dd13aed3b0
Ekene <ekeneazubuko2935@gmail.com>
2025-07-14 23:31:06 -0700

    Modified redeploy script

--- app/scripts/redeploy-site.sh
+++ app/scripts/redeploy-site.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-tmux kill-server
-
-cd portfolio-site/
+cd ../..
 
 git fetch && git reset origin/main --hard
 
@@ -10,4 +8,4 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new-session -d -s flaskapp 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myportfolio
